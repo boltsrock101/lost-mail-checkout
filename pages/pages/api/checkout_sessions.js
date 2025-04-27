c601dbbfdf9cbf2196af1e9335099c816b3f1784
boltsrock101 <lostmaildotio@gmail.com>
2025-04-26 21:52:47 -0700

    Create checkout_sessions.js

---
+++ pages/pages/api/checkout_sessions.js
@@ -0,0 +1,35 @@
+import Stripe from 'stripe';
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    try {
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        mode: 'payment',
+        line_items: [
+          {
+            price_data: {
+              currency: 'usd',
+              product_data: {
+                name: 'Lost Mail Mystery Box',
+              },
+              unit_amount: 2999, // $29.99
+            },
+            quantity: 1,
+          },
+        ],
+        success_url: `${req.headers.origin}/?success=true`,
+        cancel_url: `${req.headers.origin}/?canceled=true`,
+      });
+
+      res.status(200).json({ id: session.id });
+    } catch (err) {
+      res.status(500).json({ statusCode: 500, message: err.message });
+    }
+  } else {
+    res.setHeader('Allow', 'POST');
+    res.status(405).end('Method Not Allowed');
+  }
+}
